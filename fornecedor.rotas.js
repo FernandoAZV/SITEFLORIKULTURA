76f94a0dbcad71bdd2014f4cb1354dc628674dd6
FernandoAZV <65910849+FernandoAZV@users.noreply.github.com>
2020-05-27 18:55:55 -0300

    Update fornecedor.rotas.js

--- fornecedor.rotas.js
+++ fornecedor.rotas.js
@@ -11,14 +11,14 @@ router.get('/listarfornecedor', function(req, res){
 
 //localhost:3000/api/fornecedor/listarnomefornec
 router.get('/listarpornomefornec/:nomefornec', function(req, res){
-    res.json('Oriano Glauber')
-    res.json('Oriano Glauber Junior')
-    res.json('Oriano Glauber Neto')
+  
 })
 
 //localhost:3000/api/fornecedor/listarporemail
 router.get('/listarpoemail', function(req, res){
-    res.json('lista de fornecedores com este email:')
+    res.json('orianoglauber@gmail.com')
+    res.json('orianojunior@gmail.com')
+    res.json('orianoneto@gmail.com')
 })
 
 //localhost:3000/api/fornecedor/listarfornecedor
